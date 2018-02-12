582fb2e1468175ed097a669cc76947d5e7c53198
Grzegorz Kluka <g.kluka@wp.pl>
2018-02-12 13:47:39 +0100

    First commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	GOOGLE_CLIENT_ID:
+		'863546605110-us65e6sf1no2rbf4bqu0r2ef4iq75t99.apps.googleusercontent.com',
+	GOOGLE_CLIENT_SECRET: 'yhWZgwF6yRMjFI9V10yqu1ro',
+	CALLBACK_URL: 'http://localhost:3000/auth/google/callback',
+};
